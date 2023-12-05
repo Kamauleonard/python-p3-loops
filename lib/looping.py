58fb7fe5c9c6db3eac677c35f6df8c9b7a34389d
Kamauleonard <leonardmwangi2012@gmail.com>
2023-12-05 13:27:05 +0300

    First Commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+   
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+  
+
+    return [char ** 2 for char in int_list]
+  
 
 def fizzbuzz():
-    # code goes here!
-    pass
+  
+    for char in range(1, 101):
+        if char % 3 == 0 and char % 5 == 0:
+            print("FizzBuzz")
+        elif char % 3 == 0:
+            print("Fizz")
+        elif char % 5 == 0:
+            print("Buzz")
+        else:
+            print(char)
+
+
+
+
+
+
+happy_new_year()
+
+
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = square_integers(numbers)
+print(squared_numbers)
+
+
+fizzbuzz()
